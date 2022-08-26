a37123f1f81321b5e7c5c538e4722f8d6c27408a
shalev <shalevsharf@gmail.com>
2022-08-26 15:48:17 +0300

    upload

--- apt-filter.js
+++ apt-filter.js
@@ -2,7 +2,8 @@
 
 const must = (radio) => radio.filter(rb => rb.checked).length
 
-const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRooms, immediate) {
+const findRelevantApts =
+function (address, minPrice, maxPrice, minRooms, maxRooms, immediate) {
     let relevantApts = apartments.filter(a =>
         a.address.toLowerCase().includes(address.toLowerCase() || "") &&
         a.price >= (minPrice || 0) &&
